cd97359dcd9236a16e6fbec1330cadc64cd98791
SNicolas <nicolas.samy@epitech.eu>
2024-01-29 16:42:43 +0400

    step 2 and 3

---
+++ serveur.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const url = require('url');
+
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'})
+  if (req.url == ('/acceuil.html')) {
+    res.write("<!DOCTYPE html><html><body><h1>Bienvenue sur l'acceuil</h1></body></html>");
+    console.log("Vous etes a l'acceuil");
+  }
+  if (req.url == ('/Logo.html')) {
+    res.write('<!DOCTYPE html><html><body><p><img src="https://www.cidj.com/sites/default/files/styles/full_offre/public/2021-10/Epitech-Technology-logo.gif?itok=1zuGzxJz" alt="Photo de plage vue du dessus" /></p></body></html>');
+    console.log("Vous etes au logo");
+  }
+  if (req.url == ('/profile.html')) {
+    res.write("<!DOCTYPE html><html><body><h1>Bienvenue sur votre profile</h1></body></html>");
+    console.log("Bienvenue sur votre profile");
+  }
+  res.end();
+}).listen(3001);
+
+console.log("Serveur creer");
